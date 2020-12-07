0e44025f287d5b73cfc88dfc7d87dbd9fb788b7f
Tianyi Gu <2008tygu@gmail.com>
2020-12-06 20:43:40 -0500

    python - rename script - edit code for easy heavy, number the after-filtered problem from 1 to 100

--- script/renameInstance.py
+++ script/renameInstance.py
@@ -6,4 +6,4 @@ with open("/home/aifs1/gu/phd/research/workingPaper/boundedCostSearch/optimalSol
     data = json.load(f)
     for i, key in enumerate(data):
         if os.path.exists(data[key]):
-            shutil.copy2(data[key], '/home/aifs1/gu/phd/research/workingPaper/realtime-nancy/worlds/vaccumworld/'+key)
+            shutil.copy2(data[key], '/home/aifs1/gu/phd/research/workingPaper/realtime-nancy/worlds/slidingTile_tianyi1000-easy-for-heavy/'+key)
