65546b6feb6de7f092d9afa751a110b795cc3158
Tianyi Gu <2008tygu@gmail.com>
2020-12-16 11:24:18 -0500

    bash - add plot bash

---
+++ script/plot/plotGenerator.sh
@@ -0,0 +1,143 @@
+#pythonOut=$(python boundedCostPlot.py -d tile -s uniform)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s uniform -b 1)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s uniform -b 3 -e 40)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s uniform -t cpu -b 1)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s uniform -t cpu -b 3 -e 40)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s uniform -t coverageplt -b 1)
+#echo "$pythonOut"
+
+#pythonOut=$(python boundedCostPlot.py -d tile -s heavy)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s heavy -b 1)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s heavy -b 3 -e 40)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s heavy -t cpu -b 1)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s heavy -t cpu -b 3 -e 40)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s heavy -t coverageplt -b 1)
+#echo "$pythonOut"
+
+#pythonOut=$(python boundedCostPlot.py -d tile -s heavy-easy)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s heavy-easy -b 1)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s heavy-easy -b 3 -e 40)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s heavy-easy -t cpu -b 1)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s heavy-easy -t cpu -b 3 -e 40)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s heavy-easy -t coverageplt -b 1)
+#echo "$pythonOut"
+
+#pythonOut=$(python boundedCostPlot.py -d tile -s inverse)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s inverse -b 1)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s inverse -b 3 -e 40)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s inverse -t cpu -b 1)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s inverse -t cpu -b 3 -e 40)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s inverse -t coverageplt -b 1)
+#echo "$pythonOut"
+
+#pythonOut=$(python boundedCostPlot.py -d tile -s inverse-easy)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s inverse-easy -b 1)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s inverse-easy -b 3 -e 40)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s inverse-easy -t cpu -b 1)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s inverse-easy -t cpu -b 3 -e 40)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d tile -s inverse-easy -t coverageplt -b 1)
+#echo "$pythonOut"
+
+#pythonOut=$(python boundedCostPlot.py -d pancake -s regular -z 50 -e 2.4)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d pancake -s regular -z 50 -b 1 -e 2.4)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d pancake -s regular -z 50 -b 2)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d pancake -s regular -z 50 -t cpu -b 1 -e 2.4)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d pancake -s regular -z 50 -t cpu -b 2)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d pancake -s regular -z 50 -t coverageplt -b 1)
+#echo "$pythonOut"
+
+#pythonOut=$(python boundedCostPlot.py -d pancake -s heavy -z 16 -e 2.4) #need to comment out ptshat
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d pancake -s heavy -z 16 -b 1 -e 2.4) #need to comment out ptshat
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d pancake -s heavy -z 16 -b 2)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d pancake -s heavy -z 16 -t cpu -b 1 -e 2.4) #need to comment out ptshat
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d pancake -s heavy -z 16 -t cpu -b 2)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d pancake -s heavy -z 16 -t coverageplt -b 1)
+#echo "$pythonOut"
+
+#pythonOut=$(python boundedCostPlot.py -d vaccumworld -s uniform -e 2)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d vaccumworld -s uniform -b 1 -e 2)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d vaccumworld -s uniform -b 1.8)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d vaccumworld -s uniform -t cpu -b 1 -e 2)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d vaccumworld -s uniform -t cpu -b 1.8)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d vaccumworld -s uniform -t coverageplt -b 1)
+#echo "$pythonOut"
+
+pythonOut=$(python boundedCostPlot.py -d vaccumworld -s heavy -e 2.4)
+echo "$pythonOut"
+pythonOut=$(python boundedCostPlot.py -d vaccumworld -s heavy -b 1 -e 2.4)
+echo "$pythonOut"
+pythonOut=$(python boundedCostPlot.py -d vaccumworld -s heavy -b 2)
+echo "$pythonOut"
+pythonOut=$(python boundedCostPlot.py -d vaccumworld -s heavy -t cpu -b 1 -e 2.4)
+echo "$pythonOut"
+pythonOut=$(python boundedCostPlot.py -d vaccumworld -s heavy -t cpu -b 2)
+echo "$pythonOut"
+pythonOut=$(python boundedCostPlot.py -d vaccumworld -s heavy -t coverageplt -b 1)
+echo "$pythonOut"
+
+#pythonOut=$(python boundedCostPlot.py -d racetrack -s barto-bigger -e 2.4)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d racetrack -s barto-bigger -b 1 -e 2.4)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d racetrack -s barto-bigger -b 2)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d racetrack -s barto-bigger -t cpu -b 1 -e 2.4)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d racetrack -s barto-bigger -t cpu -b 2)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d racetrack -s barto-bigger -t coverageplt -b 1)
+#echo "$pythonOut"
+
+#pythonOut=$(python boundedCostPlot.py -d racetrack -s hansen-bigger -e 2.4)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d racetrack -s hansen-bigger -b 1 -e 2.4)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d racetrack -s hansen-bigger -b 2)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d racetrack -s hansen-bigger -t cpu -b 1 -e 2.4)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d racetrack -s hansen-bigger -t cpu -b 2)
+#echo "$pythonOut"
+#pythonOut=$(python boundedCostPlot.py -d racetrack -s hansen-bigger -t coverageplt -b 1)
+#echo "$pythonOut"
+
